ac6aebc7fc7420286e6eec597d01445d09a56e47
YutharsanS <yutharsansivabalan@gmail.com>
2024-10-02 06:34:55 +0530

    title change

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -96,7 +96,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>Notes</h1>
+      <h1>Notes Deploy Test</h1>
       <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)} >
